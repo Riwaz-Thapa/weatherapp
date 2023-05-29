c2ccdeb8e9257e0c2e856138f18e136473ed7180
Riwaz Thapa <reewazthapa22@gmail.com>
2023-05-29 13:02:58 +0545

    feat: accessing the data from the API and showing to user

--- script.js
+++ script.js
@@ -1,6 +1,8 @@
 const search_box = document.getElementById("search");
 const button_search = document.getElementById("button-search");
 const showCity = document.querySelector("#city");
+const Temp = document.querySelector("#temperature");
+const weather_img = document.querySelector("#image");
 const city_Status = document.querySelector("#status");
 
 async function weather(city) {
@@ -8,9 +10,16 @@ async function weather(city) {
   const url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=${api_key}&units=matrics`;
   const weather_data = await fetch(url).then((response) => response.json());
   console.log(weather_data);
-}
+  if (weather_data.cod == "404") {
+    Temp.innerHTML = `0°C`;
+    city_Status.innerHTML = "City not found"
+  }
+  Temp.innerHTML = `${Math.round(weather_data.main.temp - 273.15)}°C`;
+  city_Status.innerHTML = `${weather_data.weather[0].main}`;
+  }
 
-button_search.addEventListener('click', (e) => {
+
+button_search.addEventListener("click", (e) => {
   e.preventDefault();
   let city = search_box.value;
   weather(city);
